1bdd2b50552e3e2c0fc2e4caa40849aff4e106ea
AshKGrey <41754645+AshKGrey@users.noreply.github.com>
2019-03-14 13:32:33 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='SLA-ZTA',
+      version='3.2',
+      description='Self-Contained Labor Areas for Canada',
+      url='https://github.com/CSBP-CPSE/SLA-ZTA',
+      author='Ash Munro',
+      author_email='anne.munro@canada.ca',
+      license='MIT',
+      packages=['SLA'],
+      zip_safe=False)
